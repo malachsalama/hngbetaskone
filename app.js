4a16c88c32f2256869934259a19f435bed423ec0
Salama <malach.salama@gmail.com>
2023-09-11 22:05:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const dayOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({
+    slack_name: req.query.slack_name,
+    current_day: dayOfWeek[new Date().getDay()],
+    utc_time: new Date()
+      .toISOString()
+      .replace(/\d{2}.\d{3,}(?=Z$)/, (num) =>
+        Number(num).toFixed(0).padStart(2, 0)
+      ),
+    track: req.query.track,
+    github_file_url: "",
+    github_repo_url: "",
+    status_code: req.statusCode,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
